c37465f791c6ae61790a371f6782b8cd59fea95c
sinduramu <sinduramu@gmail.com>
2023-07-13 14:45:47 +0800

    push the changes

---
+++ InterviewPrograms/src/main/java/seleniumprograms/TestJava.java
@@ -0,0 +1,11 @@
+package seleniumprograms;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test for Git push");
+
+	}
+
+}
